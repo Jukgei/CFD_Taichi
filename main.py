8780c6dd4cb300f1f63de0d3d8f6b81d69a3ed3b
jukgei <jukgei@outlook.com>
2022-11-26 23:56:41 +0800

    add box volume

--- main.py
+++ main.py
@@ -19,14 +19,31 @@ particle_radius = 0.1
 particle_num = int(water_size.x / particle_radius * water_size.y / particle_radius * water_size.z / particle_radius)
 logger.info("Particle count: %d", particle_num)
 
-start_pos = ti.Vector([0, 0, 0])
+start_pos = ti.Vector([1, 1, 1])
+
+box_min = ti.Vector([0, 0, 0])
+box_max =  ti.Vector([3, 3, 3])
 
 pos = ti.Vector.field(3, ti.f32, shape=particle_num)
 vel = ti.Vector.field(3, ti.f32, shape=particle_num)
 acc = ti.Vector.field(3, ti.f32, shape=particle_num)
 
 
+box_vert = ti.Vector.field(3, ti.f32, shape=12)
 
+box_vert[0] = ti.Vector([box_min.x, box_min.y, box_min.z])
+box_vert[1] = ti.Vector([box_min.x, box_max.y, box_min.z])
+box_vert[2] = ti.Vector([box_max.x, box_min.y, box_min.z])
+box_vert[3] = ti.Vector([box_max.x, box_max.y, box_min.z])
+box_vert[4] = ti.Vector([box_min.x, box_min.y, box_max.z])
+box_vert[5] = ti.Vector([box_min.x, box_max.y, box_max.z])
+box_vert[6] = ti.Vector([box_max.x, box_min.y, box_max.z])
+box_vert[7] = ti.Vector([box_max.x, box_max.y, box_max.z])
+
+box_lines_indices = ti.field(int, shape=(2 * 12))
+
+for i, val in enumerate([0, 1, 0, 2, 1, 3, 2, 3, 4, 5, 4, 6, 5, 7, 6, 7, 0, 4, 1, 5, 2, 6, 3, 7]):
+	box_lines_indices[i] = val
 
 @ti.kernel
 def init_particle():
@@ -39,10 +56,6 @@ def init_particle():
 		z = int(i / xy_num)
 		pos[i] = (ti.Vector([x, y, z]) + start_pos) * particle_radius * 2
 
-	# for i in range(water_size.x):
-	# 	for j in range(water_size.y):
-	# 		for k in range(water_size.z):
-	# 			x =
 
 
 particles_pos = ti.Vector.field(3, dtype=ti.f32, shape=N)
@@ -62,9 +75,14 @@ if __name__ == "__main__":
 	camera.lookat(-5,-2,-2)
 	# camera.lookat(0,-1,0)
 	camera.up(0,1,0)
-	# camera.projection_mode(mode)
+	# ti.ui.ProjectionMode = 0
+	# camera.projection_mode(ti.ui.ProjectionMode)
 	scene.set_camera(camera)
+
 	init_particle()
+
+
+
 	while window.running:
 		camera.track_user_inputs(window, movement_speed=0.005, hold_key=ti.ui.RMB)
 		scene.set_camera(camera)
@@ -72,6 +90,6 @@ if __name__ == "__main__":
 		scene.point_light(pos=(0.5, 1.5, 1.5), color=(1, 1, 1))
 
 		scene.particles(pos, color=(0.68, 0.26, 0.19), radius=particle_radius)
-
+		scene.lines(box_vert, indices=box_lines_indices, color=(0.99, 0.68, 0.28), width=1.0)
 		canvas.scene(scene)
 		window.show()
