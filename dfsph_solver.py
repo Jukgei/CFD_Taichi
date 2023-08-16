1e35fde8929c1b84f69b0521da1e3206e2d6394f
jukgei <jukgei@outlook.com>
2023-08-16 14:50:29 +0800

    [DFSPH]: fix bug: correct alpha formula

--- dfsph_solver.py
+++ dfsph_solver.py
@@ -1,6 +1,7 @@
 import taichi as ti
 from solver_base import solver_base
 
+
 class dfsph_solver(solver_base):
 
 	def __init__(self, particle_system, config):
@@ -10,6 +11,7 @@ class dfsph_solver(solver_base):
 		self.rho_adv = ti.field(ti.float32, shape=self.particle_count)
 		self.rho_derivative = ti.field(ti.float32, shape=self.particle_count)
 		self.vel_adv = ti.Vector.field(3, ti.float32, shape=self.particle_count)
+		self.vel_adv_delta = ti.Vector.field(3, ti.float32, shape=self.particle_count)
 		self.force_ext = ti.Vector.field(3, ti.float32, shape=self.particle_count)
 		self.force_ext.fill(self.gravity * ti.Vector([0.0, -1.0, 0.0]) * self.ps.particle_m)
 
@@ -26,7 +28,11 @@ class dfsph_solver(solver_base):
 			square_sum = 0.0
 			self.ps.for_all_neighbor(i, self.compute_sum, sum_square)
 			self.ps.for_all_neighbor(i, self.compute_square_sum, square_sum)
-			self.alpha[i] = sum_square.dot(sum_square) + square_sum
+			denominator = (sum_square.dot(sum_square) + square_sum)
+			if ti.abs(denominator) < 1e-6:
+				self.alpha[i] = 0.0
+			else:
+				self.alpha[i] = self.rho[i] / denominator
 
 	@ti.func
 	def compute_sum(self, i, j):
@@ -47,16 +53,21 @@ class dfsph_solver(solver_base):
 
 	@ti.kernel
 	def compute_all_vel_adv(self):
+		max_vel = -ti.math.inf
 		for i in range(self.particle_count):
 			self.vel_adv[i] = self.ps.vel[i] + self.delta_time * self.force_ext[i] / self.ps.particle_m
+			ti.atomic_max(max_vel, self.vel_adv[i].norm())
+		if self.delta_time > 0.4 * self.ps.particle_radius * 2 / max_vel:
+			print('WARNING! DELTA TIME TOO SMALL. According to CLF condition, delta time must larger than {}'.format(0.4 * self.ps.particle_radius * 2/ max_vel))
 
 	@ti.kernel
 	def compute_all_rho_adv(self) -> ti.float32:
 		rho_avg = 0.0
 		for i in range(self.particle_count):
-			rho_adv = 0.0
-			self.ps.for_all_neighbor(i, self.compute_rho_adv, rho_adv)
-			self.rho_adv[i] = self.rho[i] + self.delta_time * rho_adv
+			delta = 0.0
+			self.ps.for_all_neighbor(i, self.compute_rho_adv, delta)
+			self.rho_adv[i] = ti.max(self.rho[i] + self.delta_time * delta, self.rho_0)
+			# self.rho_adv[i] = self.rho[i] + self.delta_time * delta
 			rho_avg += self.rho_adv[i]
 		return rho_avg / self.particle_count
 
@@ -71,16 +82,16 @@ class dfsph_solver(solver_base):
 		for i in range(self.particle_count):
 			vel_adv = ti.Vector([0.0, 0.0, 0.0])
 			self.ps.for_all_neighbor(i, self.iter_vel_adv, vel_adv)
-			self.vel_adv[i] -= vel_adv * self.delta_time
+			self.vel_adv_delta[i] = vel_adv
+		for i in range(self.particle_count):
+			self.vel_adv[i] -= self.vel_adv_delta[i] * self.delta_time
 
 	@ti.func
 	def iter_vel_adv(self, i, j):
-		# todo camp to zero?
 		k_i = (self.rho_adv[i] - self.rho_0) * self.alpha[i] / self.delta_time_2
 		k_j = (self.rho_adv[j] - self.rho_0) * self.alpha[j] / self.delta_time_2
 		q = self.ps.pos[i] - self.ps.pos[j]
-		ret = self.ps.particle_m * (k_i / self.rho[i] + k_j / self.rho[j]) * self.cubic_kernel_derivative(q, self.kernel_h)
-		return ret
+		return self.ps.particle_m * (k_i / self.rho[i] + k_j / self.rho[j]) * self.cubic_kernel_derivative(q, self.kernel_h)
 
 	def correct_density_error(self):
 		rho_avg = ti.math.inf
@@ -100,57 +111,47 @@ class dfsph_solver(solver_base):
 	def compute_all_position(self):
 		for i in range(self.particle_count):
 			self.ps.pos[i] = self.ps.pos[i] + self.delta_time * self.vel_adv[i]
+			self.ps.vel[i] = self.vel_adv[i]
 
 		for i in range(self.particle_count):
 			for j in ti.static(range(3)):
 				if self.ps.pos[i][j] <= self.ps.box_min[j] + self.ps.particle_radius:
 					self.ps.pos[i][j] = self.ps.box_min[j] + self.ps.particle_radius
-					self.vel_adv[i][j] *= -self.v_decay_proportion
+					self.ps.vel[i][j] *= -self.v_decay_proportion
 
 				if self.ps.pos[i][j] >= self.ps.box_max[j] - self.ps.particle_radius:
 					self.ps.pos[i][j] = self.ps.box_max[j] - self.ps.particle_radius
-					self.vel_adv[i][j] *= -self.v_decay_proportion
+					self.ps.vel[i][j] *= -self.v_decay_proportion
 
 	@ti.kernel
-	def update_density(self):
-		self.compute_all_rho()
-
-	@ti.kernel
-	def update_alpha(self):
-		self.compute_all_alpha()
-
-	@ti.kernel
-	def iter_all_rho_derivative(self) -> ti.float32:
+	def derivative_iter_all_rho(self) -> ti.float32:
 		avg = 0.0
 		for i in range(self.particle_count):
 			rho_derivative = 0.0
 			self.ps.for_all_neighbor(i, self.compute_rho_derivative, rho_derivative)
-			self.rho_derivative[i] = rho_derivative
-			avg += rho_derivative
+			self.rho_derivative[i] = ti.max(rho_derivative, 0.0)
+			# self.rho_derivative[i] = -self.rho[i] * rho_derivative
+			avg += self.rho_derivative[i]
 		return avg / self.particle_count
 
 	@ti.func
 	def compute_rho_derivative(self, i, j):
-		# todo: check formula again & has a same function
 		q = self.ps.pos[i] - self.ps.pos[j]
 		return self.ps.particle_m * (self.vel_adv[i] - self.vel_adv[j]).dot(self.cubic_kernel_derivative(q, self.kernel_h))
 
 	@ti.kernel
-	def iter_all_vel_adv_about_divergence(self):
+	def divergence_iter_all_vel_adv(self):
 		for i in range(self.particle_count):
-
 			vel_adv = ti.Vector([0.0, 0.0, 0.0])
-			self.ps.for_all_neighbor(i, self.iter_vel_adv_about_divergence, vel_adv)
+			self.ps.for_all_neighbor(i, self.divergence_iter_vel_adv, vel_adv)
 			self.vel_adv[i] -= vel_adv * self.delta_time
 
 	@ti.func
-	def iter_vel_adv_about_divergence(self, i, j):
-		# todo camp to zero?
-		k_i = (self.rho_derivative[i] * self.alpha[i] / self.delta_time)
-		k_j = (self.rho_derivative[j] * self.alpha[j] / self.delta_time)
+	def divergence_iter_vel_adv(self, i, j):
+		k_i = self.rho_derivative[i] * self.alpha[i] / self.delta_time
+		k_j = self.rho_derivative[j] * self.alpha[j] / self.delta_time
 		q = self.ps.pos[i] - self.ps.pos[j]
-		ret = self.ps.particle_m * (k_i / self.rho[i] + k_j / self.rho[j]) * self.cubic_kernel_derivative(q,
-																										  self.kernel_h)
+		ret = self.ps.particle_m * (k_i / self.rho[i] + k_j / self.rho[j]) * self.cubic_kernel_derivative(q, self.kernel_h)
 		return ret
 
 	def correct_divergence_error(self):
@@ -159,18 +160,13 @@ class dfsph_solver(solver_base):
 
 		while iter_cnt < self.min_iteration_density_divergence or rho_divergence_avg > self.density_divergence_threshold:
 
-			rho_divergence_avg = self.iter_all_rho_derivative()
+			rho_divergence_avg = self.derivative_iter_all_rho()
 
-			self.iter_all_vel_adv_about_divergence()
+			self.divergence_iter_all_vel_adv()
 
 			iter_cnt += 1
 
 			print('[divergence iteration] count: {}, error {}'.format(iter_cnt, rho_divergence_avg))
-
-	@ti.kernel
-	def update_velocity(self):
-		for i in range(self.particle_count):
-			self.ps.vel[i] = self.vel_adv[i]
 
 	@ti.kernel
 	def initialize(self):
@@ -178,6 +174,9 @@ class dfsph_solver(solver_base):
 		self.compute_all_alpha()
 
 	def iterate(self):
+
+		self.correct_divergence_error()
+
 		self.compute_all_ext_force()
 
 		self.compute_all_vel_adv()
@@ -185,18 +184,6 @@ class dfsph_solver(solver_base):
 		self.correct_density_error()
 
 		self.compute_all_position()
-
-		self.ps.reset_grid()
-
-		self.ps.update_grid()
-
-		self.update_density()
-
-		self.update_alpha()
-
-		self.correct_divergence_error()
-
-		self.update_velocity()
 
 	def step(self):
 		super(dfsph_solver, self).step()
