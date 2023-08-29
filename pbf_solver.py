110f182f7cb6ee3ebc8d407827992c02712e2bac
jukgei <jukgei@outlook.com>
2023-08-29 22:24:01 +0800

    [PBF]: add 2012Akinci method to handle the boundary problem

--- pbf_solver.py
+++ pbf_solver.py
@@ -16,11 +16,12 @@ class pbf_solver(solver_base):
 		self.pbf_lambda = ti.field(ti.float32, shape=particle_count)
 		self.epsilon = 1.0e-6
 
-		self.k = 1e-6 	# tension
-		self.c = 5e-6 	# viscosity
+		self.k = 1e-7 	# tension
+		self.c = 9e-6 	# viscosity
 		self.s_corr_factor = 0.3
 
-
+		# self.k = 1e-7  # tension
+		# self.c = 1e-5  # viscosity
 
 	@ti.kernel
 	def externel_force_predict_pos(self):
@@ -38,10 +39,15 @@ class pbf_solver(solver_base):
 			if self.constrain[i] == 0.0:
 				self.pbf_lambda[i] = 0.0
 			else:
-				sum = 0.0
 
+				sum = 0.0
 				self.ps.for_all_neighbor(i, self.compute_derivative_around_sum, sum)
-				sum = self.constrain_derivative[i].dot(self.constrain_derivative[i]) + sum
+				if self.boundary_handle == self.akinci2012_boundary_handle:
+					sum_boundary = 0.0
+					self.ps.for_all_boundary_neighbor(i, self.compute_derivative_around_sum_boundary, sum_boundary)
+					sum = self.constrain_derivative[i].dot(self.constrain_derivative[i]) + sum + sum_boundary
+				else:
+					sum = self.constrain_derivative[i].dot(self.constrain_derivative[i]) + sum
 
 				self.pbf_lambda[i] = - self.constrain[i] / (sum + self.epsilon)
 
@@ -51,7 +57,12 @@ class pbf_solver(solver_base):
 		for i in range(self.particle_count):
 			delta_pos = ti.Vector([0.0, 0.0, 0.0])
 			self.ps.for_all_neighbor(i, self.compute_delta_pos, delta_pos)
-			self.delta_pos[i] = delta_pos / self.rho_0
+			if self.boundary_handle == self.akinci2012_boundary_handle:
+				delta_pos_boundary = ti.Vector([0.0, 0.0, 0.0])
+				self.ps.for_all_boundary_neighbor(i, self.compute_delta_pos_boundary, delta_pos_boundary)
+				self.delta_pos[i] = (delta_pos + delta_pos_boundary) / self.rho_0
+			else:
+				self.delta_pos[i] = delta_pos / self.rho_0
 
 	@ti.kernel
 	def update_all_pos(self):
@@ -60,14 +71,15 @@ class pbf_solver(solver_base):
 			self.ps.fluid_particles.vel[i] = (self.pos_predict[i] - self.ps.fluid_particles.pos[i]) / self.delta_time[None]
 
 			# for i in range(self.particle_count):
-			for j in ti.static(range(3)):
-				if self.pos_predict[i][j] <= self.ps.box_min[j] + self.ps.particle_radius:
-					self.pos_predict[i][j] = self.ps.box_min[j] + self.ps.particle_radius
-					self.ps.fluid_particles.vel[i][j] *= self.v_decay_proportion
+			if self.boundary_handle == self.clamp_boundary_handle:
+				for j in ti.static(range(3)):
+					if self.pos_predict[i][j] <= self.ps.box_min[j] + self.ps.particle_radius:
+						self.pos_predict[i][j] = self.ps.box_min[j] + self.ps.particle_radius
+						self.ps.fluid_particles.vel[i][j] *= self.v_decay_proportion
 
-				if self.pos_predict[i][j] >= self.ps.box_max[j] - self.ps.particle_radius:
-					self.pos_predict[i][j] = self.ps.box_max[j] - self.ps.particle_radius
-					self.ps.fluid_particles.vel[i][j] *= self.v_decay_proportion
+					if self.pos_predict[i][j] >= self.ps.box_max[j] - self.ps.particle_radius:
+						self.pos_predict[i][j] = self.ps.box_max[j] - self.ps.particle_radius
+						self.ps.fluid_particles.vel[i][j] *= self.v_decay_proportion
 
 			# self.ps.fluid_particles.vel[i] = (self.pos_predict[i] - self.ps.fluid_particles.pos[i]) / self.delta_time[None]
 			self.ps.fluid_particles.pos[i] = self.pos_predict[i]
@@ -75,22 +87,42 @@ class pbf_solver(solver_base):
 
 			v = ti.Vector([0.0, 0.0, 0.0])
 			self.ps.for_all_neighbor(i, self.update_vel, v)
-			self.ps.fluid_particles.vel[i] += self.c * v
+			if self.boundary_handle == self.akinci2012_boundary_handle:
+				v_boundary = ti.Vector([0.0, 0.0, 0.0])
+				self.ps.for_all_boundary_neighbor(i, self.update_vel_boundary, v_boundary)
+				# self.ps.fluid_particles.vel[i] += self.c * (v + v_boundary)
+				self.ps.fluid_particles.vel[i] += self.c * v
+			else:
+				self.ps.fluid_particles.vel[i] += self.c * v
 
 	@ti.func
 	def update_vel(self, i, j):
 		return (self.ps.fluid_particles.vel[j] - self.ps.fluid_particles.vel[i] ) * self.poly_kernel((self.ps.fluid_particles.pos[i] - self.ps.fluid_particles.pos[j]).norm(), self.kernel_h)
 
 	@ti.func
+	def update_vel_boundary(self, i, j):
+		return self.ps.fluid_particles.vel[j] * self.poly_kernel((self.ps.fluid_particles.pos[i] - self.ps.boundary_particles.pos[j]).norm(), self.kernel_h)
+
+	@ti.func
 	def compute_all_constrain_derivative(self):
 		for i in range(self.particle_count):
 			constrain_derivative = ti.Vector([0.0, 0.0, 0.0])
 			self.ps.for_all_neighbor(i, self.compute_constrain_derivative, constrain_derivative)
-			self.constrain_derivative[i] = constrain_derivative
+			if self.boundary_handle == self.akinci2012_boundary_handle:
+				constrain_derivative_boundary = ti.Vector([0.0, 0.0, 0.0])
+				self.ps.for_all_boundary_neighbor(i, self.compute_constrain_derivative_boundary, constrain_derivative_boundary)
+				self.constrain_derivative[i] = constrain_derivative + constrain_derivative_boundary
+			else:
+				self.constrain_derivative[i] = constrain_derivative
 
 	@ti.func
 	def compute_constrain_derivative(self, i, j) -> ti.types.vector:
 		return self.spiky_kernel_derivative(self.ps.fluid_particles.pos[i] - self.ps.fluid_particles.pos[j], self.kernel_h) / self.rho_0
+
+	@ti.func
+	def compute_constrain_derivative_boundary(self, i, j) -> ti.types.vector:
+		return self.spiky_kernel_derivative(self.ps.fluid_particles.pos[i] - self.ps.boundary_particles.pos[j],
+											self.kernel_h) / self.rho_0
 
 	@ti.func
 	def compute_all_constrain(self):
@@ -101,6 +133,12 @@ class pbf_solver(solver_base):
 	def compute_derivative_around_sum(self, i, j):
 
 		ret = self.spiky_kernel_derivative(self.ps.fluid_particles.pos[i] - self.ps.fluid_particles.pos[j], self.kernel_h) / self.rho_0
+		return ret.dot(ret)
+
+	@ti.func
+	def compute_derivative_around_sum_boundary(self, i, j):
+
+		ret = self.spiky_kernel_derivative(self.ps.fluid_particles.pos[i] - self.ps.boundary_particles.pos[j], self.kernel_h) / self.rho_0
 		return ret.dot(ret)
 
 	@ti.func
@@ -115,15 +153,25 @@ class pbf_solver(solver_base):
 		return (self.pbf_lambda[i] + self.pbf_lambda[j] + s_corr) * self.spiky_kernel_derivative(x_ij, self.kernel_h)
 
 	@ti.func
+	def compute_delta_pos_boundary(self, i, j) -> ti.types.vector:
+		x_ij = self.ps.fluid_particles.pos[i] - self.ps.boundary_particles.pos[j]
+		x_ij_norm = x_ij.norm()
+		s_corr = self.poly_kernel(x_ij_norm, self.kernel_h) / self.poly_kernel(self.s_corr_factor * self.kernel_h, self.kernel_h)
+		s_corr *= s_corr
+		s_corr *= s_corr
+		s_corr *= -self.k
+
+		return (self.pbf_lambda[i] + s_corr) * self.spiky_kernel_derivative(x_ij, self.kernel_h)
+
+	@ti.func
 	def compute_rho(self, i, j):
 		return self.ps.particle_m * self.poly_kernel((self.ps.fluid_particles.pos[i] - self.ps.fluid_particles.pos[j]).norm(), self.kernel_h)
 
 	@ti.func
-	def compute_all_rho(self):
-		for i in range(self.particle_count):
-			rho = 0.001
-			self.ps.for_all_neighbor(i, self.compute_rho, rho)
-			self.rho[i] = rho
+	def compute_rho_from_boundary(self, i, j):
+		q = (self.ps.fluid_particles.pos[i] - self.ps.boundary_particles.pos[j]).norm()
+		ret = self.ps.boundary_particles.volume[j] * self.poly_kernel(q, self.kernel_h)
+		return ret
 
 	def step(self):
 
