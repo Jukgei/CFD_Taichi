7cd14728a195685c90b4acd867d205b7037a399f
jukgei <jukgei@outlook.com>
2023-09-03 21:50:27 +0800

    [Rigid Solver]: add rotation collision detect (just consider x-axis).

--- rigid_solver.py
+++ rigid_solver.py
@@ -38,7 +38,8 @@ class rigid_solver:
 			self.ps.rigid_particles.force[i] = ti.Vector([0.0, 0.0, 0.0])
 
 		alpha = self.ps.rigid_inertia_tensor[None] @ torque
-		alpha = ti.Vector([2e-1, 0.0, 0.0])
+		if self.simulate_cnt[None] == 1:
+			alpha = ti.Vector([-2, 0.0, 0.0])
 		# m = ti.math.rotation3d(0.0, 0.0, 0.0)
 		# m = ti.math.rot_yaw_pitch_roll(1.0 / 180.0 * ti.math.pi, 0, 0)
 		# m[0, 3] = -self.ps.rigid_centriod[None].x
@@ -51,13 +52,21 @@ class rigid_solver:
 		# m = ti.math.rotation3d(self.omega[None].x, self.omega[None].z, self.omega[None].y)
 		# m = ti.math.rotation3d(self.attitude[None].x, self.attitude[None].z, self.attitude[None].y)
 		m = ti.math.rotation3d(self.attitude[None].x, self.attitude[None].z, self.attitude[None].y)
-		max_x = self.rotation_collision()
-		if 0 < max_x < self.attitude[None].x:
-			m = ti.math.rotation3d(max_x, self.attitude[None].z, self.attitude[None].y)
-			self.omega[None].x *= -self.v_decay_proportion
-			print('Collision! omega {}, attitude {}'.format(self.omega[None].x, self.attitude[None].x))
-		elif max_x <= 0:
-			m = ti.math.rotation3d(0.0, self.attitude[None].z, self.attitude[None].y)
+		x_rotation_constrain = self.rotation_collision(self.attitude[None])
+		print('x_rotation constrain {}, attitude {}'.format(x_rotation_constrain, self.attitude[None].x))
+		if self.attitude[None].x > 0:
+			if x_rotation_constrain.x < self.attitude[None].x:
+				m = ti.math.rotation3d(x_rotation_constrain.x, self.attitude[None].z, self.attitude[None].y)
+				self.omega[None].x *= -self.v_decay_proportion
+				print('Collision! omega {}, attitude {}'.format(self.omega[None].x, self.attitude[None].x))
+		elif self.attitude[None].x < 0:
+			if x_rotation_constrain.y > self.attitude[None].x:
+				m = ti.math.rotation3d(x_rotation_constrain.y, self.attitude[None].z, self.attitude[None].y)
+				self.omega[None].x *= -self.v_decay_proportion
+				print('Collision!! omega {}, attitude {}'.format(self.omega[None].x, self.attitude[None].x))
+
+		# elif max_x <= 0:
+		# 	m = ti.math.rotation3d(0.0, self.attitude[None].z, self.attitude[None].y)
 
 		for i in range(self.particle_count):
 			self.ps.rigid_particles.pos[i] -= self.ps.rigid_centriod[None]
@@ -106,7 +115,7 @@ class rigid_solver:
 		self.ps.rigid_centriod[None] = centroid / sum_mass
 
 	@ti.func
-	def rotation_collision(self) -> ti.f32:
+	def rotation_collision(self, attitude: ti.math.vec3) -> ti.math.vec2:
 		x_min_particle = self.ps.rigid_particles[0]
 		x_max_particle = self.ps.rigid_particles[0]
 		y_min_particle = self.ps.rigid_particles[0]
@@ -145,7 +154,7 @@ class rigid_solver:
 			if y_min == delta_position.y:
 				y_min_particle = particle
 
-			ti.atomic_max(z_min, delta_position.z)
+			ti.atomic_min(z_min, delta_position.z)
 			if z_min == delta_position.z:
 				z_min_particle = particle
 
@@ -160,27 +169,131 @@ class rigid_solver:
 			pass
 
 		# Just consider rotation around the x-axis
-		y_d = self.ps.rigid_particles.rgb[y_max_particle.index] - self.ps.rigid_centriod[None]
+		y_d = self.ps.rigid_particles.pos[y_max_particle.index] - self.ps.rigid_centriod[None]
 		y_d.x = 0
 		y_rad_max = ti.math.acos(y_max / y_d.norm())
 		y_rad_min = ti.math.acos(center2boundary_max.y / y_d.norm())
 		y_rad = y_rad_max - y_rad_min
+		y_rad_com = 2 * ti.math.pi - 2 * y_rad_min - y_rad
+		z = ti.sqrt(y_d.norm() ** 2 - center2boundary_max.y ** 2)
+		# Don't change sign
+		vec1 = ti.Vector([y_d.z, y_d.y])
+		vec2 = ti.Vector([-z, center2boundary_min.y])
+		if attitude.x < 0:
+			vec2 = ti.Vector([z, center2boundary_min.y])
+		clockwise = self.clockwise_orientation(vec1, vec2)
+		y_rotation_clockwise = y_rad
+		y_rotation_clockwise_inv = -y_rad_com
+		if clockwise == 1:
+			y_rotation_clockwise = y_rad
+			y_rotation_clockwise_inv = - y_rad_com
+		elif clockwise == -1:
+			y_rotation_clockwise = y_rad_com
+			y_rotation_clockwise_inv = - y_rad
 
-		z_d = self.ps.rigid_particles.rgb[z_max_particle.index] - self.ps.rigid_centriod[None]
+		y_d_min = self.ps.rigid_particles.pos[y_min_particle.index] - self.ps.rigid_centriod[None]
+		y_d_min.x = 0
+		ymin_rad_max = ti.math.acos(ti.abs(y_min) / y_d_min.norm())
+		ymin_rad_min = ti.math.acos(ti.abs(center2boundary_min.y) / y_d_min.norm())
+		ymin_rad = ymin_rad_max - ymin_rad_min
+		ymin_rad_com = 2 * ti.math.pi - 2 * ymin_rad_min - ymin_rad
+		z = ti.sqrt(y_d_min.norm() ** 2 - center2boundary_min.y ** 2)
+		# change sign
+		vec1 = ti.Vector([-y_d_min.z, y_d_min.y])
+		vec2 = ti.Vector([z, center2boundary_min.y])
+		if attitude.x < 0:
+			vec2 = ti.Vector([-z, center2boundary_min.y])
+		clockwise = self.clockwise_orientation(vec1, vec2)
+		ymin_rotation_clockwise = ymin_rad
+		ymin_rotation_clockwise_inv = -ymin_rad_com
+		if clockwise == 1:
+			ymin_rotation_clockwise = ymin_rad
+			ymin_rotation_clockwise_inv = - ymin_rad_com
+		elif clockwise == -1:
+			ymin_rotation_clockwise = ymin_rad_com
+			ymin_rotation_clockwise_inv = - ymin_rad
+
+
+		z_d = self.ps.rigid_particles.pos[z_max_particle.index] - self.ps.rigid_centriod[None]
 		z_d.x = 0
 		z_rad_max = ti.math.acos(z_max / z_d.norm())
 		z_rad_min = ti.math.acos(center2boundary_max.z / z_d.norm())
 		z_rad = z_rad_max - z_rad_min
-		print('y rad{}, z rad{}'.format(y_rad_max - y_rad_min, z_rad_max - z_rad_min))
+		z_rad_com = 2 * ti.math.pi - 2 * z_rad_min - z_rad
+		y = ti.sqrt(z_d.norm() ** 2 - center2boundary_max.z ** 2)
+		# Don't change sign
+		vec1 = ti.Vector([-z_d.z, z_d.y])
+		vec2 = ti.Vector([-center2boundary_max.z, y])
+		if attitude.x < 0:
+			vec2 = ti.Vector([-center2boundary_max.z, -y])
+		clockwise = self.clockwise_orientation(vec1, vec2)
+		# print('clockwise: {}, vec1 {}, vec2 {}'.format(clockwise, vec1, vec2))
+		z_rotation_clockwise = z_rad
+		z_rotation_clockwise_inv = -z_rad_com
+		if clockwise == 1:
+			z_rotation_clockwise = z_rad
+			z_rotation_clockwise_inv = - z_rad_com
+		elif clockwise == -1:
+			z_rotation_clockwise = z_rad_com
+			z_rotation_clockwise_inv = - z_rad
 
-		if z_max > center2boundary_max.z:
-			self.ps.rigid_particles.rgb[z_max_particle.index] = ti.Vector([1.0, 1.0, 1.0])
-			# print('Collison !')
-		else:
-			self.ps.rigid_particles.rgb[z_max_particle.index] = ti.Vector([1.0, 1.0, 0.0])
+		z_d_min = self.ps.rigid_particles.pos[z_min_particle.index] - self.ps.rigid_centriod[None]
+		z_d_min.x = 0
+		zmin_rad_max = ti.math.acos(ti.abs(z_min) / z_d_min.norm())
+		zmin_rad_min = ti.math.acos(ti.abs(center2boundary_min.z) / z_d_min.norm())
+		zmin_rad = zmin_rad_max - zmin_rad_min
+		zmin_rad_com = 2 * ti.math.pi - 2 * zmin_rad_min - zmin_rad
+		y = ti.sqrt(z_d_min.norm() ** 2 - center2boundary_min.z ** 2)
+		# Don't change sign
+		vec1 = ti.Vector([-z_d_min.z, z_d_min.y])
+		vec2 = ti.Vector([-center2boundary_min.z, y])
+		if attitude.x < 0:
+			vec2 = ti.Vector([-center2boundary_min.z, -y])
+		clockwise = self.clockwise_orientation(vec1, vec2)
+		# print('clockwise: {}, vec1 {}, vec2 {}'.format(clockwise, vec1, vec2))
+		zmin_rotation_clockwise = zmin_rad
+		zmin_rotation_clockwise_inv = -zmin_rad_com
+		if clockwise == 1:
+			zmin_rotation_clockwise = zmin_rad
+			zmin_rotation_clockwise_inv = - zmin_rad_com
+		elif clockwise == -1:
+			zmin_rotation_clockwise = zmin_rad_com
+			zmin_rotation_clockwise_inv = - zmin_rad
+
+
+		# if z_min < center2boundary_min.z:
+		# 	self.ps.rigid_particles.rgb[z_min_particle.index] = ti.Vector([1.0, 1.0, 1.0])
+		# 	# print('Collison !')
+		# else:
+		# 	self.ps.rigid_particles.rgb[z_min_particle.index] = ti.Vector([1.0, 1.0, 0.0])
 			# print('center2boundary_max.y {}, y_max {}'.format(center2boundary_max.y, y_max))
+		x_max = ti.min(y_rotation_clockwise, z_rotation_clockwise, zmin_rotation_clockwise, ymin_rotation_clockwise)
+		x_min = ti.max(y_rotation_clockwise_inv, z_rotation_clockwise_inv, zmin_rotation_clockwise_inv, ymin_rotation_clockwise_inv)
+		# print(y_rad, z_rad, zmin_rad, ymin_rad)
+		# print('clock wise {}, {}'.format(y_rotation_clockwise, ymin_rotation_clockwise))
+		# print('clock wise inv {}, {}'.format(y_rotation_clockwise_inv, ymin_rotation_clockwise_inv))
+		return ti.Vector([x_max, x_min])
 
-		return ti.min(y_rad, z_rad)
+	@ti.func
+	def clockwise_orientation(self, vec1: ti.math.vec2, vec2: ti.math.vec2) -> ti.i32:
+		# d = ti.math.dot(vec1, vec2)
+		# c = ti.math.cross(vec1, vec2).norm()
+		# ret = 0
+		# if d > 0 and c > 0:
+		# 	ret = -1
+		# elif d < 0 and c > 0:
+		# 	ret = 1
+		ret = 0
+		c = ti.math.cross(vec1, vec2)
+		if c > 0:
+			ret = -1
+		elif c < 0:
+			ret = 1
+		else:
+			pass
+			# print('Error!')
+		return ret
+
 
 	@ti.kernel
 	def reset(self):
