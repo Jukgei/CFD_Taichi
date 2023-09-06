fc8ccb7ce28bac89e9957ce70813b2ec0c1cc746
jukgei <jukgei@outlook.com>
2023-09-07 00:38:57 +0800

    [Rigid Solver]: 1. use the impulse method handle rigid solver problem 2. fix bug: if the torque is negative, object rotates counterclockwise

--- rigid_solver.py
+++ rigid_solver.py
@@ -21,12 +21,7 @@ class rigid_solver:
 		self.omega = ti.field(ti.math.vec3, shape=())
 		self.attitude = ti.field(ti.math.vec3, shape=())
 
-		self.v_decay_proportion = 0.9
-
-		self.omega_decay_proportion = 0.0
-
-		self.temp_pos = ti.field(ti.math.vec3, shape=self.particle_count)
-		self.collision_iteration_cnt = 5
+		self.v_decay_proportion = 0.5
 
 	@ti.kernel
 	def kinematic(self):
@@ -43,31 +38,89 @@ class rigid_solver:
 		# self.ps.rigid_particles.vel.fill()
 		vel = self.ps.rigid_particles.acc[0] * self.delta_time[None] + self.ps.rigid_particles.vel[0]
 		displacement = vel * self.delta_time[None]
-
+		ori_displacement = vel * self.delta_time[None]
 		# self.displacement_distance = -ti.math.inf
 		# displacement_min = displacement
+		collision_point_cnt = 0
+		collision_point = ti.Vector([0.0, 0.0, 0.0])
+		collision_particle_v = ti.Vector([0.0, 0.0, 0.0])
+		collision_norm = ti.Vector([0.0, 0.0, 0.0])
+		collision_mass = 0.0
 		for i in range(self.particle_count):
 			for j in ti.static(range(3)):
-				if self.ps.rigid_particles.pos[i][j] + displacement[j] <= self.ps.box_min[j] + self.ps.particle_radius:
+				collision = 0
+				if self.ps.rigid_particles.pos[i][j] + ori_displacement[j] <= self.ps.box_min[j] + self.ps.particle_diameter:
 					# self.ps.rigid_particles.rgb[i] = ti.Vector([1.0, 1.0, 1.0])
-					ti.atomic_max(displacement[j], self.ps.box_min[j] + self.ps.particle_radius - self.ps.rigid_particles.pos[i][j])
-					ti.atomic_max(vel[j], - vel[j] * self.v_decay_proportion)
+					ti.atomic_max(displacement[j], self.ps.box_min[j] + self.ps.particle_diameter - self.ps.rigid_particles.pos[i][j])
+					v = vel + ti.math.cross(self.omega[None], (self.ps.rigid_particles.pos[i] + ori_displacement - self.ps.rigid_centriod[None]))
+					if v[j] < 0:
+						collision = 1
+						collision_particle_v += v
+						collision_norm[j] = -1
+					self.ps.rigid_particles.rgb[i] = ti.Vector([1.0, 1.0, 0.0])
 
-				if self.ps.rigid_particles.pos[i][j] + displacement[j] >= self.ps.box_max[j] - self.ps.particle_radius:
+					# ti.atomic_max(vel[j], - vel[j] * self.v_decay_proportion)
+
+				if self.ps.rigid_particles.pos[i][j] + ori_displacement[j] >= self.ps.box_max[j] - self.ps.particle_diameter:
 					# self.ps.rigid_particles.rgb[i] = ti.Vector([1.0, 1.0, 1.0])
-					ti.atomic_min(displacement[j], self.ps.box_max[j] - self.ps.particle_radius - self.ps.rigid_particles.pos[i][j])
-					ti.atomic_min(vel[j], - vel[j] * self.v_decay_proportion)
+					ti.atomic_min(displacement[j], self.ps.box_max[j] - self.ps.particle_diameter - self.ps.rigid_particles.pos[i][j])
+					v = vel + ti.math.cross(self.omega[None], (self.ps.rigid_particles.pos[i] + ori_displacement - self.ps.rigid_centriod[None]))
+					if v[j] > 0:
+						collision = 1
+						collision_particle_v += v
+						collision_norm[j] = 1
+					self.ps.rigid_particles.rgb[i] = ti.Vector([1.0, 1.0, 0.0])
+
+				if collision == 1:
+					# collision_point += (self.ps.rigid_particles.pos[i] + ori_displacement - self.ps.rigid_centriod[None]) * self.ps.rigid_particles.mass[i]
+					collision_point += (self.ps.rigid_particles.pos[i] - self.ps.rigid_centriod[None])# * self.ps.rigid_particles.mass[i]
+					collision_point_cnt += 1
+					collision_mass += self.ps.rigid_particles.mass[i]
+
+		collision_particle_v_new = ti.Vector([0.0, 0.0, 0.0])
+
+		if collision_point_cnt > 0:
+			collision_point = (collision_point + ori_displacement) / collision_point_cnt
+			collision_particle_v /= collision_point_cnt
+
+			collision_particle_v_new = self.compute_new_vel(collision_particle_v, collision_norm)
+
+		sum_mass = 0.0
+		for i in range(self.ps.rigid_particles_num):
+			sum_mass += self.ps.rigid_particles.mass[i]
+
+		if collision_point_cnt > 0:
+			collision_point_matrix = ti.math.mat3([[0.0, - collision_point.z, collision_point.y], [collision_point.z, 0.0, -collision_point.x], [-collision_point.y, collision_point.x, 0.0]])
+			K = ti.Matrix.identity(ti.f32, 3) / sum_mass - collision_point_matrix @ ti.math.inverse(self.ps.rigid_inertia_tensor[None]) @ collision_point_matrix
+
+
+			inv_K = ti.math.inverse(K)
+			j = inv_K @ (collision_particle_v_new - collision_particle_v)
+			vel += j / sum_mass
+			self.omega[None] += ti.math.inverse(self.ps.rigid_inertia_tensor[None]) @ ti.math.cross(collision_point, j)
 
 		self.ps.rigid_particles.vel.fill(vel)
 		for i in range(self.particle_count):
 			self.ps.rigid_particles.pos[i] += displacement
 
 		centroid = ti.Vector([0.0, 0.0, 0.0])
-		sum_mass = 0.0
+
 		for i in range(self.ps.rigid_particles_num):
 			centroid += self.ps.rigid_particles.pos[i] * self.ps.rigid_particles.mass[i]
-			sum_mass += self.ps.rigid_particles.mass[i]
+
 		self.ps.rigid_centriod[None] = centroid / sum_mass
+
+	@ti.func
+	def compute_new_vel(self, v, n) -> ti.math.vec3:
+		mu_t = 0.8 # friction
+		mu_n = self.v_decay_proportion
+		v_n = v.dot(n) * n
+		v_t = v - v_n
+		a = ti.max(1 - mu_t * (1 + mu_n) * v_n.norm() / v_t.norm(), 0.0)
+		v_n_new = - mu_n * v_n
+		v_t_new = a * v_t
+		v_new = v_t_new + v_n_new
+		return v_new
 
 	@ti.kernel
 	def compute_attitude(self):
@@ -75,83 +128,20 @@ class rigid_solver:
 		for i in range(self.particle_count):
 			pos = self.ps.rigid_particles.pos[i] - self.ps.rigid_centriod[None]
 			torque += ti.math.cross(pos, self.ps.rigid_particles.force[i])
-		alpha = self.ps.rigid_inertia_tensor[None] @ torque
+
+		alpha = ti.math.inverse(self.ps.rigid_inertia_tensor[None]) @ torque
 		self.omega[None] += alpha * self.delta_time[None]
 		self.attitude[None] = self.omega[None] * self.delta_time[None]
 
 	@ti.kernel
-	def _iterate_collision(self, rad: ti.f32, axis: ti.i32) -> ti.int32:
-		m = ti.math.mat4(1)
-		for i in range(self.particle_count):
-			self.temp_pos[i] = self.ps.rigid_particles.pos[i]
-			self.temp_pos[i] -= self.ps.rigid_centriod[None]
-		if axis == 0:
-			m = ti.math.rotation3d(rad, 0.0, 0.0)
-		elif axis == 1:
-			m = ti.math.rotation3d(0.0, 0.0, rad)
-		elif axis == 2:
-			m = ti.math.rotation3d(0.0, rad, 0.0)
-
-		for i in range(self.particle_count):
-			pos = self.temp_pos[i]
-			pos4 = ti.Vector([pos.x, pos.y, pos.z, 1])
-			pos4 = m @ pos4
-			self.temp_pos[i] = ti.Vector([pos4.x, pos4.y, pos4.z])
-
-		for i in range(self.particle_count):
-			self.temp_pos[i] += self.ps.rigid_centriod[None]
-
-		is_collision = 0
-		for i in range(self.particle_count):
-			for j in ti.static(range(3)):
-				if self.temp_pos[i][j] <= self.ps.box_min[j] + self.ps.particle_radius:
-					is_collision = 1
-				if self.temp_pos[i][j] >= self.ps.box_max[j] - self.ps.particle_radius:
-					is_collision = 1
-		return is_collision
-
-	def rotate_collision(self, axis):
-		assert 0 <= axis < 3, "axis must larger than 0 and less than 3"
-		rad = self.attitude[None][axis]
-		is_collision = 0
-		iter_cnt = 0
-		while iter_cnt < self.collision_iteration_cnt:
-			is_collision = self._iterate_collision(rad, axis)
-			# print('iter count {}, is collision {}'.format(iter_cnt, is_collision))
-			if is_collision == 0 and iter_cnt == 0:
-				break
-			if is_collision == 1:
-				rad /= 2
-			elif is_collision == 0:
-				rad += 0.5 * rad
-			iter_cnt += 1
-
-		if is_collision == 1:
-			self.attitude[None][axis] = rad / 2
-		elif is_collision == 0:
-			self.attitude[None][axis] = rad
-		if iter_cnt > 0:
-			# print('collision ! axis {}'.format(axis))
-			self.omega[None][axis] *= -self.omega_decay_proportion
-
-	@ti.kernel
 	def rotation(self):
-		m = ti.math.rotation3d(self.attitude[None].x, self.attitude[None].z, self.attitude[None].y)
+		m = ti.math.rotation3d(-self.attitude[None].x, -self.attitude[None].z, -self.attitude[None].y)
+		R = ti.math.mat3([[m[0, 0], m[0, 1], m[0, 2]], [m[1, 0], m[1, 1], m[1, 2]], [m[2, 0], m[2, 1], m[2, 2]]])
 
 		for i in range(self.particle_count):
-			self.ps.rigid_particles.pos[i] -= self.ps.rigid_centriod[None]
+			self.ps.rigid_particles.pos[i] = R @(self.ps.rigid_particles.pos[i] - self.ps.rigid_centriod[None]) + self.ps.rigid_centriod[None]
 
-		for i in range(self.particle_count):
-			pos = self.ps.rigid_particles.pos[i]
-			pos4 = ti.Vector([pos.x, pos.y, pos.z, 1])
-			pos4 = m @ pos4
-			self.ps.rigid_particles.pos[i] = ti.Vector([pos4.x, pos4.y, pos4.z])
-
-		for i in range(self.particle_count):
-			self.ps.rigid_particles.pos[i] += self.ps.rigid_centriod[None]
-
-		# Artificial rotation friction
-		self.omega[None] *= 0.99
+		self.ps.rigid_inertia_tensor[None] = R @ self.ps.rigid_inertia_tensor[None] @ R.transpose()
 
 	@ti.kernel
 	def reset(self):
@@ -166,10 +156,6 @@ class rigid_solver:
 		self.reset()
 
 		self.compute_attitude()
-
-		self.rotate_collision(0)
-		self.rotate_collision(1)
-		self.rotate_collision(2)
 
 		self.rotation()
 
