63b69891eacac60bd1b06b0d9a2795300b2780b2
jukgei <jukgei@outlook.com>
2023-09-14 22:54:37 +0800

    output based on frame rate

--- main.py
+++ main.py
@@ -73,19 +73,23 @@ if __name__ == "__main__":
 	frame_cnt = 0
 	iter_cnt = solver_config.get('iter_cnt')
 
-	video_manager = ti.tools.VideoManager(output_dir="./output", framerate=60, automatic_build=False)
-
 	np_rgba = np.reshape(ps.rgba.to_numpy(), (ps.particle_num, 4))
 	series_prefix = './output/output'
 	is_output_gif = scene_config.get('is_output_gif', False)
 	is_output_ply = scene_config.get('is_output_ply', False)
 	output_fps = scene_config.get('output_fps', 60)
+	frame_time = 1.0 / output_fps
+	video_manager = ti.tools.VideoManager(output_dir="./output", framerate=output_fps, automatic_build=False)
 	is_pause = not scene_config.get('is_simulate', True)
 	flag = 0
 	render_fluid = True
 	render_rigid = True
+	output_frame_cnt = 0
 	ply_cnt = 0
 	t = 0.0
+	simulation_time_consuming = 0
+	pause_start_time = 0
+	pause_time = 0
 	while window.running:
 		# Debug GUI
 
@@ -103,13 +107,19 @@ if __name__ == "__main__":
 		gui.text("frame_cnt: {}".format(frame_cnt))
 		gui.text("delta time: {:.5f}".format(solver.delta_time[None]))
 		gui.text("time: {:.4f}".format(t))
-		gui.text("real time: {:.3f}".format(time.time() - start_time))
+		if is_pause:
+			gui.text("real time: {:.3f}".format(pause_start_time - pause_time - start_time))
+		else:
+			gui.text("real time: {:.3f}".format(time.time() - start_time - pause_time))
+		gui.text("ply_cnt: {}".format(ply_cnt))
 		gui.text("Pause: {}".format(is_pause))
 		if window.is_pressed(ti.GUI.ESCAPE):
 			break
-		if window.is_pressed(ti.GUI.SPACE):
+		if window.is_pressed(ti.GUI.SPACE) and is_pause:
+			pause_time += time.time() - pause_start_time
 			is_pause = False
-		if window.is_pressed('p'):
+		if window.is_pressed('p') and not is_pause:
+			pause_start_time = time.time()
 			is_pause = True
 		if window.is_pressed('f'):
 			render_fluid = True
@@ -140,7 +150,7 @@ if __name__ == "__main__":
 			scene.particles(ps.fluid_particles.pos, color=(0.0, 0.28, 1), radius=ps.particle_radius, per_vertex_color=ps.fluid_particles.rgb)
 		if render_rigid and config.get('solid', {}):
 			scene.particles(ps.rigid_particles.pos, color=(1.0, 0.0, 0), radius=ps.particle_radius / 5, per_vertex_color=ps.rigid_particles.rgb)
-		scene.particles(rs.debug_particles, color=(1.0, 1.0, 1.0), radius=ps.particle_radius)
+		# scene.particles(rs.debug_particles, color=(1.0, 1.0, 1.0), radius=ps.particle_radius)
 		# scene.particles(ps.boundary_particles.pos, color=(1.0, 1.0, 1.0), radius=ps.particle_radius)
 
 		if not is_pause:
@@ -162,22 +172,27 @@ if __name__ == "__main__":
 
 		# gui.show(f'frame/{frame_cnt:06d}.png')
 
-		if is_output_gif:
+		if is_output_gif and (t / frame_time) > output_frame_cnt:
 			img = window.get_image_buffer_as_numpy()
 			video_manager.write_frame(img)
+			output_frame_cnt += 1
 
-		if is_output_ply:
+		if is_output_ply and (t / frame_time) > ply_cnt:
 			np_pos = np.reshape(ps.fluid_particles.pos.to_numpy(), (ps.particle_num, 3))
 			writer = ti.tools.PLYWriter(num_vertices=ps.particle_num)
 			writer.add_vertex_pos(np_pos[:, 0], np_pos[:, 1], np_pos[:, 2])
 			writer.add_vertex_rgba(
 				np_rgba[:, 0], np_rgba[:, 1], np_rgba[:, 2], np_rgba[:, 3])
 			writer.export_frame_ascii(ply_cnt, series_prefix)
+			ps.update_mesh_vextics()
+			with open(f"output/obj_{ply_cnt:06}.obj", "w") as f:
+				e = ps.mesh.export(file_type='obj')
+				f.write(e)
 			ply_cnt += 1
 		# print(frame_cnt)
 		window.show()
 
 	if is_output_gif:
-		video_manager.make_video(gif=True, mp4=False)
+		video_manager.make_video(gif=True, mp4=True)
 
 	print("Simulation time: {}".format(time.time() - start_time))
